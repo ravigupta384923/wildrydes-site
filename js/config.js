25c2c93ebf0f0ad247755bc3523f56ad7e6f596e
Ravi Gupta <ravigupta384923@gmail.com>
2021-07-15 13:21:37 -0700

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_y3Ej2SsE6', // e.g. eu-east-2_uXboG5pAb
+        userPoolClientId: '39c9j29mq5v80o6bckb800aubp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
